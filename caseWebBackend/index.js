9bc18bd233f153fdf51148fc17da2ea0c734ad18
FCJar <Fcjardim764@gmail.com>
2025-05-07 09:42:59 -0300

    0.0.3

--- caseWebBackend/index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./App");
-const Loaders = require("./src/Loaders/index");
-
-
-Loaders.start();
-
-app.listen(8000, () => console.log("Servidor Rodando"));
--- caseWebBackend/src/Loaders/mongodb.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-async function startDB() {
-    await mongoose.connect('mongodb+srv://filipej:lHXdJH2YuI5ZM5Lq@cluster0.w3iyfmt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-}
-
-module.exports = startDB;
